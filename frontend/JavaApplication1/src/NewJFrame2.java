783e3a3abcae3337d32d26d161b6b6907db1402a
ipalakchopra <palak.chopra@somaiya.edu>
2023-05-03 23:04:16 +0530

    sbmt bttn

--- frontend/JavaApplication1/src/NewJFrame2.java
+++ frontend/JavaApplication1/src/NewJFrame2.java
@@ -43,10 +43,10 @@ public class NewJFrame2 extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jDesktopPane1 = new javax.swing.JDesktopPane();
         jLabel1 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
         jTextField3 = new javax.swing.JTextField();
         jButton3 = new javax.swing.JButton();
         jTextField4 = new javax.swing.JTextField();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -101,17 +101,11 @@ public class NewJFrame2 extends javax.swing.JFrame {
                     .addGap(90, 90, 90)))
         );
 
-        jButton2.setFont(new java.awt.Font("Roboto", 2, 18)); // NOI18N
-        jButton2.setText("SUBMIT");
-        jButton2.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 7, true));
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-
         jTextField3.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
         jTextField3.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         jTextField3.setText("Upload the key file");
         jTextField3.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 0, 0), 7, true));
 
-        jButton3.setBackground(new java.awt.Color(255, 255, 255));
         jButton3.setFont(new java.awt.Font("Roboto", 2, 18)); // NOI18N
         jButton3.setText("UPLOAD");
         jButton3.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 0, 0), 7, true));
@@ -126,6 +120,15 @@ public class NewJFrame2 extends javax.swing.JFrame {
         jTextField4.setText("File Name");
         jTextField4.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 0, 0), 7, true));
         jTextField4.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        jButton4.setFont(new java.awt.Font("Roboto Medium", 0, 20)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(102, 0, 0));
+        jButton4.setText("SUBMIT");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -151,11 +154,11 @@ public class NewJFrame2 extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(344, 344, 344))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(287, 287, 287))))
+                        .addGap(287, 287, 287))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(322, 322, 322))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -176,9 +179,9 @@ public class NewJFrame2 extends javax.swing.JFrame {
                     .addComponent(jTextField3)
                     .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(73, 73, 73)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(70, 70, 70))
+                .addGap(71, 71, 71)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(76, 76, 76))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -221,10 +224,7 @@ public class NewJFrame2 extends javax.swing.JFrame {
         File f=chooser.getSelectedFile();
         try{
             File fl = new File(f.getAbsolutePath());
-            //fl = FileIO.read(new File(f.getAbsolutePath()));
-            //Image img1 = img.getScaledInstance(jLabel1.getWidth(), jLabel1.getHeight(), Image.SCALE_SMOOTH);
-            //ImageIcon format = new ImageIcon(img1);
-            //jLabel1.setIcon(format);
+  
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, e,"Error",JOptionPane.ERROR_MESSAGE);
         }
@@ -233,15 +233,15 @@ public class NewJFrame2 extends javax.swing.JFrame {
         jTextField4.setText(filename);
     }//GEN-LAST:event_jButton3ActionPerformed
 
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // submit
+    }//GEN-LAST:event_jButton4ActionPerformed
+
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -298,16 +298,8 @@ public class NewJFrame2 extends javax.swing.JFrame {
 				e.getStackTrace();
 			}
 
-			/*System.out.println(row);
-			System.out.println(row_key.length);
-			System.out.println(" ");*/
 			for(int col = 0; col<w_c; col++){
 
-				/*System.out.println(row);
-				System.out.println(col);
-				System.out.println(row_key[col]);
-				
-				System.out.println(" ");*/
 				rgb_base = base.getRGB(Integer.parseInt(row_key[col]), row);
 				
 
@@ -346,8 +338,8 @@ public class NewJFrame2 extends javax.swing.JFrame {
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
